9a4970ba55b253bdda24bddee5c18e5159d0aa34
Alejo Boga <alejoboga20@gmail.com>
2022-09-22 15:26:02 -0400

    Get specific joke

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,10 +1,33 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import { db } from '~/utils/db.server';
+
+export const loader: LoaderFunction = async ({ params }) => {
+	const joke = await db.joke.findUnique({
+		where: { id: params.jokeId },
+		select: { id: true, name: true, content: true },
+	});
+	if (!joke) throw new Error('Joke not found');
+
+	const data: LoaderData = { joke };
+
+	return json(data);
+};
+
 const JokeRoute = () => {
+	const { joke } = useLoaderData<LoaderData>();
 	return (
 		<div>
 			<p>Here's your hilarious joke:</p>
-			<p>Why don't you find hippopotamuses hiding in trees? They're really good at it.</p>
+			<p>{joke.content}</p>
+			<Link to='.'>{joke.name} Permalink</Link>
 		</div>
 	);
 };
 
 export default JokeRoute;
+
+type LoaderData = {
+	joke: { name: string; content: string; id: string };
+};
