c28984746fc18b11ebf9253b273aff321f1bdcf3
Alejo Boga <alejoboga20@gmail.com>
2022-09-27 11:44:21 -0400

    Create a user session when login is succes

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,6 +3,7 @@ import { json } from '@remix-run/node';
 import { useActionData, Link, useSearchParams } from '@remix-run/react';
 
 import { db } from '~/utils/db.server';
+import { login, createUserSession } from '~/utils/session.server';
 import stylesUrl from '~/styles/login.css';
 
 export const links: LinksFunction = () => {
@@ -73,13 +74,17 @@ export const action: ActionFunction = async ({ request }) => {
 
 	switch (loginType) {
 		case 'login': {
-			// login to get the user
-			// if there's no user, return the fields and a formError
-			// if there is a user, create their session and redirect to /jokes
-			return badRequest({
-				fields,
-				formError: 'Not implemented',
-			});
+			const user = await login({ username, password });
+			console.log({ user });
+
+			if (!user) {
+				return badRequest({
+					fields,
+					formError: 'Username/Password combination is incorrect',
+				});
+			}
+
+			return createUserSession(user.id, redirectTo);
 		}
 		case 'register': {
 			const userExists = await db.user.findFirst({
--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,5 +1,12 @@
+import { createCookieSessionStorage, redirect } from '@remix-run/node';
 import bcrypt from 'bcryptjs';
 import { db } from './db.server';
+
+const sessionSecret = process.env.SESSION_SECRET;
+
+if (!sessionSecret) {
+	throw new Error('SESSION_SECRET must be set');
+}
 
 export const login = async ({ username, password }: LoginForm) => {
 	const user = await db.user.findUnique({ where: { username } });
@@ -11,6 +18,32 @@ export const login = async ({ username, password }: LoginForm) => {
 	if (!isCorrectPassword) return null;
 
 	return { id: user.id, username };
+};
+
+const storage = createCookieSessionStorage({
+	cookie: {
+		name: 'RJ_session',
+		// normally you want this to be `secure: true`
+		// but that doesn't work on localhost for Safari
+		// https://web.dev/when-to-use-local-https/
+		secure: process.env.NODE_ENV === 'production',
+		secrets: [sessionSecret],
+		sameSite: 'lax',
+		path: '/',
+		maxAge: 60 * 60 * 24 * 30,
+		httpOnly: true,
+	},
+});
+
+export const createUserSession = async (userId: string, redirectTo: string) => {
+	const session = await storage.getSession();
+	session.set('userId', userId);
+
+	return redirect(redirectTo, {
+		headers: {
+			'Set-Cookie': await storage.commitSession(session),
+		},
+	});
 };
 
 type LoginForm = {
